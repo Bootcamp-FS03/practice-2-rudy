16d560101836462fd14a43396d7c82b36bf996a4
RsarabiaJala <rudy.sarabia@jala.university>
2023-12-30 17:52:46 -0300

    improved game performance, UX, UI

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,8 +11,6 @@
 // DESIRABLE
 // 6. ADD THE ABILITY TO SELECT THE TABLE DIMENSION 3X3, 4X4, 5X5, ETC
 
-
-
 let board = [];
 const settings = {};
 
@@ -24,30 +22,33 @@ const generateBoardBox = (x, y) => {
 };
 //True for player1
 let currentPlayer = true;
+const getCurrentPlayerName = () =>
+  currentPlayer ? settings.player1 : settings.player2;
+document
+  .getElementById("gameStartSettings")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
 
-document.getElementById("gameStartSettings").addEventListener("submit", function(e){
-  e.preventDefault();
+    const formData = new FormData(this);
+    formData.forEach((value, key) => {
+      settings[key] = value;
+    });
+    console.log(settings);
 
-  const formData = new FormData(this);
-  formData.forEach((value, key) => {
-    settings[key] = value;
+    if (invalidSettings(settings)) {
+      alert("invalidInput");
+      document.forms["gameStartSettings"].reset();
+    } else {
+      processSettingsAndStartGame(settings);
+    }
   });
-  console.log(settings)
 
-  if (invalidSettings(settings)) {
-    alert("invalidInput");
-    document.forms["gameStartSettings"].reset();
-  } else {
-    processSettingsAndStartGame(settings);
-  }
-});
-
-const invalidSettings = (settings)=>{
-    return settings.player1 == "" || settings.player2 == "" || tableSize < 3
-}
+const invalidSettings = (settings) => {
+  return settings.player1 == "" || settings.player2 == "" || tableSize < 3;
+};
 
 const processSettingsAndStartGame = () => {
-  let { tableSize } = settings;
+  let { tableSize, player1, player2 } = settings;
   for (let i = 0; i < tableSize; i++) {
     board.push(Array.from({ length: tableSize }, () => null));
     for (let j = 0; j < tableSize; j++) {
@@ -55,47 +56,64 @@ const processSettingsAndStartGame = () => {
     }
   }
   document.getElementById("gameStartSettings").hidden = true;
-  document.getElementById("boardArea").hidden = false;
+  document.getElementById("gameArea").style["display"] = "flex";
 
-  document.getElementById("boardArea").style["grid-template-columns"] = `repeat(${tableSize}, minmax(100px, 1fr))`;
-  document.getElementById("boardArea").style["grid-auto-rows"] = "minmax(100px, auto);" /* Set the minimum height */
+  document.getElementById("gameInfoArea").innerHTML = `<h1>Tic tac toe</h1>
+            <div id="player1Name">${player1} <b>X</b></div>
+            <div id="player2Name">${player2} <b>O</b></div>
+            <h2 id="currentTurnPlayer">${getCurrentPlayerName()}'s Turn</h2>`;
 
+  document.getElementById("boardArea").style[
+    "grid-template-columns"
+  ] = `repeat(${tableSize}, minmax(0, 1fr))`;
 };
 
 //TODO: change alert for popup
 const makeMove = async (element, x, y) => {
   const mark = currentPlayer ? "X" : "O";
-  const currentPlayerName = currentPlayer?settings.player1:settings.player2;
   element.innerText = mark;
-  board[x][y] = mark; 
+  element.classList.add("noHover");
+  element.removeAttribute("onclick");
+  board[x][y] = mark;
 
-  await new Promise(resolve => requestAnimationFrame(resolve));
+  await new Promise((resolve) => requestAnimationFrame(resolve));
 
-  if( checkWinner(mark,board))
-    { 
-        alert(`${currentPlayerName} WINS!!!!!`) 
-        window.location.reload();
-        return;
-    } 
+  if (checkWinner(mark, board)) {
+    displayEndgame(getCurrentPlayerName())
+    return;
+  }
+  if (board.every((item) => item.every((rowItem) => rowItem))) {
+    displayEndgame()
+    return;
+  }
   currentPlayer = !currentPlayer;
-  alert(`${currentPlayerName}${currentPlayerName.endsWith("s")?"'":"'s"} turn:`)
+  document.getElementById(
+    "currentTurnPlayer"
+  ).innerText = `${getCurrentPlayerName()}'s Turn`;
 };
 
-const checkWinner = (mark, board = [[null,null,null],[null,null,null],[null,null,null]]) => {
+const checkWinner = (
+  mark,
+  board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ]
+) => {
   //board is always square
   let tableSize = board.length;
   //horizontal
-  if (board.some(row=> row.every((rowItem) => rowItem == mark)))
-    return true;
-  //vertical 
-  if (board.some(
-    (item,idx)=> 
-        board
-        .map(box=>box[idx]) //Column (BEWARE: idx refers to current row, but works as board is square, item is unused)
+  if (board.some((row) => row.every((rowItem) => rowItem == mark))) return true;
+  //vertical
+  if (
+    board.some((item, idx) =>
+      board
+        .map((box) => box[idx]) //Column (BEWARE: idx refers to current row, but works as board is square, item is unused)
         .every((colItem) => colItem == mark)
-  ))
-    return true
-  
+    )
+  )
+    return true;
+
   i = -1;
   //diagonals
 
@@ -123,6 +141,33 @@ const checkWinner = (mark, board = [[null,null,null],[null,null,null],[null,null
   return false;
 };
 
+const displayEndgame = (winner) => {
+  let finalMessage = "";
+  if (winner) finalMessage = `${winner} WINS!!!`;
+  else finalMessage = "IT'S A TIE!!!";
+
+  let boardItems =  document.getElementsByClassName("ttt_box")
+ 
+  for( let i = 0 ; i< boardItems.length; i++){
+    boardItems.item(i).classList.add("noHover");
+    boardItems.item(i).removeAttribute("onclick");
+  }
+
+  console.log(finalMessage);
+  let popup = document.createElement("div");
+  popup.setAttribute("id", "gameOverPopupContainer");
+  popup.setAttribute("onClick","reloadPage()")
+  popup.innerHTML = `<div id="gameOverPopup">
+    <h1 id="gameOverPopupTitle">Game over!</h1>
+    <h3>The winner is ${finalMessage}</h3>
+    <button onclick="reloadPage()">Sart new Game!</button>
+</div>`;
+  document.body.appendChild(popup);
+};
+
+const reloadPage = ()=>{
+    window.location.reload()
+}
 /*Test data
 
 let board = [["X","X","X"],[null, "O", "O"],["X","O","O"]];
